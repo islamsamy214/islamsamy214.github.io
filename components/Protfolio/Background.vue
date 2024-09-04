fbc6417b3e5c5ad24a96d25dce1f860ff42bdbf4
islamsamy214 <islamsamy214@gmail.com>
2024-09-04 18:35:16 +0400

    chore: Update spelling of "Protfolio" to "Portfolio" in Background.vue

--- components/Protfolio/Background.vue
+++ components/Protfolio/Background.vue
@@ -2,6 +2,6 @@
   <span
     class="fixed opacity-15 -z-10 min-w-screen min-h-screen top-20 md:right-64 uppercase text-6xl md:text-9xl font-black"
   >
-    Protfolio</span
+    Portfolio</span
   >
 </template>
