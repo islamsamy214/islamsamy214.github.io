b9e8ab2930453d3958af4bf5e550f33d5545416a
islamsamy214 <islamsamy214@gmail.com>
2024-10-18 11:36:23 +0400

    chore: Update resume file name in Hero.vue

--- components/About/Hero.vue
+++ components/About/Hero.vue
@@ -18,7 +18,7 @@ const { profile } = useProfile();
         {{ profile.intro }}
       </p>
       <a
-        href="/files/islamsamyResume.pdf"
+        href="/files/islamSamyCareer.pdf"
         target="_blank"
         download
         class="text-lg md:text-xl text-emerald-500 cursor-pointer"
